9f418e60b349bcce30a8d65281df1a1d6279a341
Nicklas Di Pietro <Nicklasdipietro@gmail.com>
2018-05-19 14:12:41 -0700

    Completed, JS. Numbers seem high? Working on CSS next.

--- js/app.js
+++ js/app.js
@@ -1,146 +1,91 @@
 'use strict';
-// The below is for 1st and Pike
 
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+var allStores = [];
 
-// var cookieTable = document.getElementById('pats-table');
-var allCookiesArray = [];
-var storesArray = [];
-var cookieTable = document.getElementById('cookieTable')
-
-//constructors always start with capital. Only thing usually
-//maxcustomers = ...Parameter helps to differentiate
-
-function CookieStore(minCustomersParameter, maxCustomersParameter, averageCookiesParameter, nameParameter) {
-
-  this.minCustomers = minCustomersParameter; // this ia a value ex. 30
-  this.maxCustomers = maxCustomersParameter;
-  this.averageCookies = averageCookiesParameter;
-  this.name = nameParameter;
-  
+function CookieStore(minCustomers, maxCustomers, averageCookies, name) {
+  this.minCustomers = minCustomers;
+  this.maxCustomers = maxCustomers;
+  this.averageCookies = averageCookies;
+  this.name = name;
   this.totalCookies = 0;
   this.customersPerHour = [];
   this.cookiesPerHour = [];
 
-  storesArray.push(this);
-
-  for () {
-
-  }
+  allStores.push(this);
 }
 
-// CMD + D to multi select
-
-
-CookieStore.prototype.populateCustomers = function () {
-  for (var i = 0; i < hours.length; i++) {
-    this.customersPerHour.push(randomCustomers(this.minCustomers, this.maxCustomers)); //populatecustomers is pushing the array of 14 numbers up into customers per hour []
-  }
-};
-
-CookieStore.prototype.calculateCookiesPerHour = function () {
-  this.populateCustomers();
-  for (var i = 0; i < hours.length; i++) {
-    this.cookiesPerHour.push(Math.ceil(this.customersPerHour[i] * this.averageCookies));
-    this.totalCookies = this.totalCookies + this.cookiesPerHour[i];
-  }
-};
-
-// ulEl (unordered list, Element)
-
-CookieStore.prototype.render = function () {
-  this.calculateCookiesPerHour();
-  var ulEl = document.getElementById('firstPike');
-
-  for (var i = 0; i < 14; i++) {
-    var liEl = document.createElement('li');
-    liEl.textContent = hours[i] + ': ' + this.cookiesPerHour[i] + ' cookies.'; // This says that i moves together with hours and cookies as one unit.
-    ulEl.append(liEl);
-  }
-};
-
-// The below creates tables header and content
-// hours.prototype = 'Not a real prototype';
-// console.log(hours.prototype);
-// console.log(CookieStore.prototype);
-
-CookieStore.prototype.render = function () {
+  CookieStore.prototype.populateCustomers = function () {
+    for (var i = 0; i < hours.length; i++) {
+      this.customersPerHour.push(getRandomNumberCustomers(this.minCustomers, this.maxCustomers));
+    }
+  };
   
-  // create tr
-  var trEl = document.createElement('tr');
-  var thEl = document.createElement('th');
-  thEl.textContent = 'defaultName';
-  trEl.appendChild(thEl);
+  CookieStore.prototype.calculateCookiesPerHour = function () {
+    this.populateCustomers();
+    for (var i in hours) {
+      this.cookiesPerHour.push(Math.ceil(this.customersPerHour[i] * this.averageCookies));
+      this.totalCookies = this.totalCookies + this.cookiesPerHour[i];
+    }
+  };
+    
 
-  // create td
-  var tdEl = document.createElement('td');
-  // give td content (Name for an individual cat)
-  console.log(this.minCookies);
+  CookieStore.prototype.render = function () {
+    this.calculateCookiesPerHour();
+    var rowValues = [this.name, this.minCustomers, this.maxCustomers, this.averageCookies, this.totalCookies];
+    var itemType = 'td';
 
-  tdEl.textContent = this.minCustomers ; //Look at this
-  // append the td to the tr
-  trEl.appendChild(tdEl);
+    console.log(this.totalCookies);
+    var cookieTable = document.getElementById('cookieTable')
+    var row = document.createElement('tr');
 
-  // create td
-  tdEl = document.createElement('td');
-  // give td content 
-  tdEl.textContent = this.maxCustomersProperty;
-  // append the td to the tr
-  trEl.appendChild(tdEl);
+    for (var i in rowValues){
+      var text = rowValues[i];
+      addItemToRowWithText(row, itemType, text); 
+    }
+    cookieTable.appendChild(row);
+  }
 
-  // create td
-  tdEl = document.createElement('td');
-  // give td content 
-  tdEl.textContent = this.averageCookies;
-  // append the td to the tr
-  trEl.appendChild(tdEl);
 
-  // append the tr to the table
-  cookieTable.appendChild(trEl);
-};
+function addItemToRowWithText(row, type, text) {
+  var el = document.createElement(type);
+  el.textContent = text;
+  row.appendChild(el);
+}
 
-// Table Row is a container that should now have min, max, average
+// BELOW
 
 function makeHeaderRow() {
-  var thEl = document.createElement('th');
-  var trEl = document.createElement('tr');
-  thEl.textContent = nameParameter;
-  trEl.appendChild(thEl);
-  thEl = document.createElement('th');
-  thEl.textContent = maxCustomersParameter;
-  trEl.appendChild(thEl);
-  thEl = document.createElement('th');
-  thEl.textContent = customersPerHour;
-  trEl.appendChild(thEl);
+  var rowNames = ['Store', 'Minimum Customers', 'Maximum Customers', 'Cookies Per Sale', 'Total Cookies Sold']
+  var itemType = 'th';
 
-//   console.log(cookieTable);
-  cookieTable.appendChild(trEl);
+  var cookieTable = document.getElementById('cookieTable')
+  var row = document.createElement('tr');
+
+  for (var i in rowNames) {
+    addItemToRowWithText(row, itemType, rowNames[i])
+  }
+
+  cookieTable.appendChild(row);
 }
 
-function rendernewStore (name, minCustomers, maxcustomers) {
-   for()
-}
-
-function randomCustomers(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+function getRandomNumberCustomers(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
 };
-
-function renderallCookiesArray() {
-  for (var i in allCookiesArray) {
-    allCookiesArray[i].render();
+  
+function renderAllStores() {
+  for (var i in allStores) {
+    allStores[i].render();
   }
 }
-
-//Making new Store instances
-
-var pikePlaceMarket = new CookieStore(23, 65, 6.3, nameParameter);
-var seaTac = new CookieStore(3, 24, 1.2, nameParameter);
-var seaCenter = new CookieStore(11, 38, 3.7, nameParameter);
-var capHill = new CookieStore(20, 38, 2.3, nameParameter);
-var alki = new CookieStore(2, 16, 4.6, nameParameter);
-
+  
+var pikePlaceMarket = new CookieStore(23, 65, 6.3, 'Pike Place Market');
+var seaTac = new CookieStore(3, 24, 1.2, 'Sea-Tac');
+var seaCenter = new CookieStore(11, 38, 3.7, 'Seattle Center');
+var capHill = new CookieStore(20, 38, 2.3, 'Capitol Hill');
+var alki = new CookieStore(2, 16, 4.6, 'Alki');
 
 makeHeaderRow();
-renderallCookiesArray();
-
-// down the line think about making an array for all the store objects
+renderAllStores();
